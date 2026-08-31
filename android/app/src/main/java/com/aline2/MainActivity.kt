756b19b813af5fb1c84634614b2429948f7e11ae
edu_creative_digital_agency <educreativedigitalagency@gmail.com>
2026-08-31 18:50:13 +0530

    Modify onCreate to handle fragment state correctly
    Override onCreate method to prevent fragment restoration issues.

--- android/app/src/main/java/com/aline2/MainActivity.kt
+++ android/app/src/main/java/com/aline2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.aline2
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +20,16 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  // Launching the native Camera app (Photo/Video capture) backgrounds this
+  // Activity, and Android can kill it to reclaim memory on low/mid RAM
+  // devices. When the user returns after confirming the photo, Android tries
+  // to recreate MainActivity and restore its saved fragment state, but
+  // react-native-screens fragments cannot be restored this way and crash
+  // with "Screen fragments should never be restored". Passing null skips
+  // that restore. See:
+  // https://github.com/software-mansion/react-native-screens/issues/17#issuecomment-424704067
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
